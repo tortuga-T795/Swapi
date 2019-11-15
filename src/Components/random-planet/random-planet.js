6f1bf703949c80382f04c21517bd07f9a583f64b
Liz <eliz.com>
2019-11-16 00:40:12 +0300

    Refactor

--- src/Components/random-planet/random-planet.js
+++ src/Components/random-planet/random-planet.js
@@ -3,52 +3,46 @@ import './random-planet.css';
 import SwapiService from '../../services/swapi-service';
 
 export default class RandomPlanet extends Component{
-    constructor(props){
-        super(props);
+    constructor(){
+        super();
         this.state = {
-            id: 2,
-            namePlanet: null,
-            population: null,
-            rotationPeriod: null,
-            diameter: null
+            planet: {}
         };
         this.updatePlanet();
     }
 
     swapiService = new SwapiService();
 
+    onPlanetLoaded = (planet)=>{
+        this.setState({planet});
+    };
+
     updatePlanet(){
         const id = Math.floor(Math.random() * 21) + 2;
-        this.swapiService.getPlanet(id).then((planet)=>{
-            this.setState({
-                id,
-                namePlanet: planet.name,
-                population: planet.population,
-                rotationPeriod: planet.rotation_period,
-                diameter: planet.diameter
-            })
-        })
-    }
+        //const id = 12;
+        this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+    };
 
     render() {
+        const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
         return(
             <div className="random-planet jumbotron rounded">
                 <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/${this.state.id}.jpg`} alt="text"/>
+                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="error"/>
                 <div>
-                    <h3>{this.state.namePlanet}</h3>
+                    <h3>{name}</h3>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">
                             <span className="term">Population</span>
-                            <span>{this.state.population}</span>
+                            <span>{population}</span>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Rotation Period</span>
-                            <span>{this.state.rotationPeriod}</span>
+                            <span>{rotationPeriod}</span>
                         </li>
                         <li className="list-group-item">
                             <span className="term">Diameter</span>
-                            <span>{this.state.diameter}</span>
+                            <span>{diameter}</span>
                         </li>
                     </ul>
                 </div>
