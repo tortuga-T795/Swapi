f7ac40c752ae0d1bfba8fb6ab4449f5754732bdd
Liz <eliz.com>
2019-11-17 21:53:10 +0300

    Error and Loading

--- src/Components/random-planet/random-planet.js
+++ src/Components/random-planet/random-planet.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import './random-planet.css';
 import SwapiService from '../../services/swapi-service';
+import Spinner from "../spinner/spinner";
+import ErrorIndicator from "../error-indicator/error-indicator";
 
 export default class RandomPlanet extends Component{
     constructor(){
         super();
         this.state = {
-            planet: {}
+            planet: {},
+            loading: true,
+            error: false
         };
         this.updatePlanet();
     }
@@ -14,38 +18,59 @@ export default class RandomPlanet extends Component{
     swapiService = new SwapiService();
 
     onPlanetLoaded = (planet)=>{
-        this.setState({planet});
+        this.setState({planet, loading: false });
+    };
+
+    onError=(error)=>{
+        this.setState({error: true, loading: false});
     };
 
     updatePlanet(){
         const id = Math.floor(Math.random() * 21) + 2;
-        this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+        this.swapiService.getPlanet(id).then(this.onPlanetLoaded).catch(this.onError);
     };
 
     render() {
-        const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
+        const {planet, loading, error} = this.state;
+        const hasData = !(loading || error);
+        const spinner = loading ? <Spinner/> : null;
+        const content = hasData ? <PlanetView planet={planet}/> : null;
+        const errorMessage = error ? <ErrorIndicator/> : null;
+
         return(
             <div className="random-planet jumbotron rounded">
-                <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="error"/>
-                <div>
-                    <h3>{name}</h3>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{population}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{rotationPeriod}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{diameter}</span>
-                        </li>
-                    </ul>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
             </div>
         );
     }
 }
+
+const PlanetView=({planet})=>{
+    const {id, name, population, rotationPeriod, diameter} = planet;
+
+    return(
+        <React.Fragment>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet image error"/>
+            <div>
+                <h3>{name}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
+};
---
+++ src/Components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner=()=>{
+    return(
+        <div className="loadingio-spinner-double-ring-pwv1kz77iur">
+            <div className="ldio-hqcu70xg95d">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Spinner;
