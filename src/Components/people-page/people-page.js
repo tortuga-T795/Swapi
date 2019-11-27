91ccd8dd81ffe25f6106610f36fc1b4245b5d7af
Liz <eliz.com>
2019-11-28 00:49:33 +0300

    Refactor

--- src/Components/people-page/people-page.js
+++ src/Components/people-page/people-page.js
@@ -4,6 +4,7 @@ import ItemList from "../item-list/item-list";
 import PersonDetails from "../person-details/person-details";
 import ErrorIndicator from "../error-indicator/error-indicator";
 import SwapiService from "../../services/swapi-service";
+import Row from "../row/row";
 
 export default class PeoplePage extends Component{
 
@@ -32,15 +33,13 @@ export default class PeoplePage extends Component{
             return <ErrorIndicator/>
         }
 
+        const itemList = <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople}
+                                   renderItem={({name, gender, birthYear})=> `${name} (${gender}, ${birthYear})`}/>;
+
+        const personDetails = <PersonDetails personId={this.state.selectedPerson}/>;
         return (
-            <div className="row mb2">
-                <div className="col-md-6">
-                    <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople}
-                              renderItem={({name, gender, birthYear})=> `${name} (${gender}, ${birthYear})`}/>
-                </div>
-                <div className="col-md-6">
-                    <PersonDetails personId={this.state.selectedPerson}/>
-                </div>
+            <div>
+                <Row left={itemList} right={personDetails}/>
             </div>
         )
     }
